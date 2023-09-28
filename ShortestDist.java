224d3be27321383672dfbf41b9eec04e318be0d5
JoelLuckett <joelluckett@icloud.com>
2023-09-28 12:00:34 +0100

    finished line curve distance function

--- ShortestDist.java
+++ ShortestDist.java
@@ -7,16 +7,16 @@ public class ShortestDist {
     //format: y=(ce1)x(^pow)+(ce2)x(^pow-1)+...+(cen-1)x+(cen)
     //each coefficient is a value in an array
     //float[] y = {3, 5 , 0, 3}; = y= 3x^3 + 5x^2 + 3
-    float[] equation1;
-    float[] equation2;
+    double[] equation1;
+    double[] equation2;
 
-    public ShortestDist(float[] _equation1, float[] _equation2) {
+    public ShortestDist(double[] _equation1, double[] _equation2) {
         this.equation1 = _equation1;
         this.equation2 = _equation2;
     }
 
     //function to format flaot arrays into readable equations
-    private String formatEquation(float[] _equation) {
+    private String formatEquation(double[] _equation) {
         DecimalFormat decimalFormat = new DecimalFormat("#.##");
         int eqLen = _equation.length;
 
@@ -27,16 +27,24 @@ public class ShortestDist {
                 if (i > 0) {
                     builder.append(" + ");
                 }
-
-                //append coefficient
-                if (_equation[i] != 1) {
-                    builder.append(decimalFormat.format(_equation[i]));
-                }
-
-                //append x and power
+                
+                //append coefficeint, x and power
                 if (i < eqLen - 1) {
+                    //append coefficient
+                    if (Math.abs(_equation[i]) != 1) {
+                        builder.append(decimalFormat.format(_equation[i]));
+                    }
+
+                    if (_equation[i] == -1) {
+                        builder.append('-');
+                    }
+
+                    //append x and power
                     builder.append('x');
                     if (eqLen - 1 - i != 1) builder.append('^').append(eqLen - 1 - i);
+                }
+                else {
+                    builder.append(decimalFormat.format(_equation[i]));
                 }
             }
         }
@@ -51,14 +59,17 @@ public class ShortestDist {
     }
 
     //function to get the distance between two lines
-    private double calcLineDist(float[] _line1, float[] _line2) {
+    private double calcLineDist(double[] _line1, double[] _line2) {
         //make sure both arrays are only of length 2
-        float[] line1 = Arrays.copyOfRange(_line1, 0, 2);
-        float[] line2 = Arrays.copyOfRange(_line2, 0, 2);
+        double[] line1 = Arrays.copyOfRange(_line1, 0, 2);
+        double[] line2 = Arrays.copyOfRange(_line2, 0, 2);
+
+        System.out.println(formatEquation(line1));
+        System.out.println(formatEquation(line2));
 
         //calculate the shortest distance between 2 lines
         if(line1[0] == line2[0]) {
-            final float grad = line1[0];
+            final double grad = line1[0];
             double dist = (Math.abs(line2[1] - line1[1])) / (Math.sqrt(1 + Math.pow(grad, 2)));
             return dist;
         }
@@ -67,20 +78,42 @@ public class ShortestDist {
     }
 
     public void printCalcLineDist() {
-        System.out.println("The distance between " + formatEquation(equation1) + " and "
+        System.out.println("The shortest distance between " + formatEquation(equation1) + " and "
         + formatEquation(equation2) + " is " + calcLineDist(equation1, equation2));
     }
 
     //function to calculate distance between a line and a curve
-    public double calcLineCurveDist(float[] _line, float[] _curve) {
+    private double calcLineCurveDist(double[] _line, double[] _curve) {
         //curve has to be a quadratic so far
-        float[] line = Arrays.copyOfRange(_line, 0, 2);
-        float[] curve = Arrays.copyOfRange(_curve, 0, 3);
+        double[] line = Arrays.copyOfRange(_line, 0, 2);
+        double[] curve = Arrays.copyOfRange(_curve, 0, 3);
 
-        float[] line1 = line;
-        // double[] line2 = {line[0], -(Math.pow(curve[1] - line[0], 2) / (4 * curve[0]) - curve[2])};
+        double a = line[0];
+        double b = line[1];
+        double c = curve[0];
+        double d = curve[1];
+        double e = curve[2];
 
-        // double dist = calcLineDist(line1, line2);
+        //get second line constant
+        final double beta = -(((Math.pow(d - a, 2)) / (4 * c)) - e);
+
+        //check if equation has solutions
+        final double det = Math.pow(d- a, 2) - 4 * c * (e - b);
+
+        if(det <= 0) {
+            //if not, solve using formula
+            double[] line1 = line;
+            double[] line2 = {line[0], beta};
+
+            double dist = calcLineDist(line1, line2);
+            return dist;
+        }
+
         return 0;
+    }
+
+    public void printCalcLineCurveDist() {
+        System.out.println("The shortest distance between " + formatEquation(equation1) + " and "
+        + formatEquation(equation2) + " is " + calcLineCurveDist(equation1, equation2));
     }
 }
--- ToolMain.java
+++ ToolMain.java
@@ -1,10 +1,10 @@
 public class ToolMain {
     public static void main(String[] args) {
-        float[] equation1 = {-1, 4};
-        float[] equation2 = {-1, 0, 3};
+        double[] equation1 = {-1, 1};
+        double[] equation2 = {-1, 0, 3};
         ShortestDist shortestDist = new ShortestDist(equation1, equation2);
 
         // shortestDist.printCalcLineDist();
-        System.out.println(shortestDist.calcLineCurveDist(equation1, equation2));
+        shortestDist.printCalcLineCurveDist();
     }
 }
